3e7e2946994d4ed38c3c9b6b930ad493f212fb78
Lucas Ferreira <casferreiraa@gmail.com>
2023-02-04 16:39:45 -0300

    feat(config): add mixins: loader and messages

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,15 +1,31 @@
+import 'package:dw9_delivery_app/app/core/ui/helpers/loader.dart';
+import 'package:dw9_delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/models/product_model.dart';
 import 'package:dw9_delivery_app/app/pages/home/widget/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppbar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // showLoader();
+          // await Future.delayed(Duration(seconds: 2));
+          // hideLoader();
+
+          showInfo("teste");
+        },
+      ),
       body: Column(
         children: <Widget>[
           Expanded(
